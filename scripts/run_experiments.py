4efa3e15fa83ecdb9bf6f253cdd73809ed407dda
sinjini08 <84672229+sinjini08@users.noreply.github.com>
2026-06-09 15:08:49 -0400

    Update run_experiments.py

--- scripts/run_experiments.py
+++ scripts/run_experiments.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 rates = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95]
-NUM_RUNS = 5
+NUM_RUNS = 3
 FILE = "data/480-360-sample.bmp"
 PORT = 9000
 OUT_FILE = "results/received_temp.bmp"
